5c703fd694481be0141ae74d5f8cd48a1e272bab
Lija321 <lisica.dejan@hotmail.com>
2022-11-16 12:58:17 +0100

    Prosao testove, TREABA ISKOMENTARISATI

--- korisnici/korisnici.py
+++ korisnici/korisnici.py
@@ -2,6 +2,38 @@ import common.konstante
 from common import konstante
 
 
+def proveri_pasos(pasos):
+    pasos=str(pasos)
+    if not all(x.isdigit() for x in pasos):
+        return True,"Pasoš nebrojevni string"
+    if len(pasos)>9:
+        return True,"Pasoš više od 9 cifara"
+    if len(pasos)<9:
+        return True,"Pasoš manje od 9 cifara"
+
+    return False,""
+
+def proveri_telefon(telefon):
+    telefon = str(telefon)
+    #print(telefon)
+    if not all(x.isdigit() for x in telefon):
+        return True, "Broj telefona nije validan"
+    return False, ""
+
+def proveri_email(email):
+    if not '@' in email:
+        return True,"Email provera bez @"
+    email=email.split('@')
+    email=email[1]
+    if email.count('.')!=1:
+        return True,"Email provera sa @ ali sa više poddomena"
+    return False,""
+
+def proveri_nedostajucu_vernost(provera_podaci: dict):
+    for key,value in provera_podaci.items():
+        if value is None: return True,f"Provera za nedostajucu vrednost: {key}"
+    return False,""
+
 
 """
 
@@ -21,40 +53,47 @@ Ova funkcija proverava i validnost podataka o korisniku, koji su tipa string.
 CHECKPOINT 1: Vraća string sa greškom ako podaci nisu validni (ne važi za konverziju brojeva).
 ODBRANA: Baca grešku sa porukom ako podaci nisu validni.
 """
+
 def kreiraj_korisnika(svi_korisnici: dict, azuriraj: bool, uloga: str, korisnicko_ime: str,
                       lozinka: str, ime: str, prezime: str, email: str = None,
                       pasos: str = None, drzavljanstvo: str = None,
                       telefon: str = None, pol: str = None) -> dict:
 
-    check_list=[[uloga,korisnicko_ime,lozinka,ime,prezime],[email,pasos,drzavljanstvo,telefon,pol]]
+    korisnik_podaci = {
+        'ime': ime,'prezime': prezime,
+        'korisnicko_ime': korisnicko_ime, 'lozinka': lozinka,
+        'email': email,
+        'pasos': str(pasos), 'drzavljanstvo': drzavljanstvo,
+        'telefon': str(telefon), 'pol': pol,
+        'uloga': uloga}
 
 
-    if all([x is None for x in check_list[1]]):
-        korisnik_podaci = {
-            'korisnicko_ime': korisnicko_ime, 'lozinka': lozinka, 'ime': ime,
-            'prezime': prezime,'uloga': uloga}
-    elif all([isinstance(x,str) for x in check_list[0]]):
-        korisnik_podaci = {
-            'korisnicko_ime': korisnicko_ime, 'lozinka': lozinka, 'ime': ime,
-            'prezime': prezime, 'email': email,
-            'pasos': pasos, 'drzavljanstvo': drzavljanstvo,
-            'telefon': telefon, 'pol': pol,
-            'uloga': uloga}
+    provera_funckije=[[proveri_nedostajucu_vernost,korisnik_podaci],
+                    [proveri_pasos,pasos],
+                      [proveri_telefon,telefon],
+                      [proveri_email,email]]
 
-    #Uvesti za gresku
-    #if not all([isinstance(x,str) for x in check_list[0]]) or (not all([isinstance(x,str) for x in check_list[1]]) or not all([isinstance(x,None) for x in check_list[0]])):
-    #    return "Greska: pogresni tipovi"
+    for provera in provera_funckije:
+        fun=provera[0]
+        prosledi=provera[1]
+        greska,poruka=fun(prosledi)
+        if greska: return poruka
+
+
+    if not (uloga ==konstante.ULOGA_PRODAVAC or uloga==konstante.ULOGA_KORISNIK or uloga==konstante.ULOGA_ADMIN):
+        print(uloga, konstante.ULOGA_KORISNIK)
+        return "Uloga nije validna"
 
     if azuriraj:
         identifiktor = korisnik_podaci['korisnicko_ime']
         if not identifiktor in svi_korisnici:
-            print("Korisnik ne postoji")
+            #print("Korisnik ne postoji")
             return "Korisnik ne postoji"
         svi_korisnici[identifiktor] = korisnik_podaci
     else:
         identifiktor=korisnik_podaci['korisnicko_ime']
         if identifiktor in svi_korisnici:
-            print("Korisnik vec postoji")
+            #print("Korisnik vec postoji")
             return "Korisnik vec postoji"
         svi_korisnici[identifiktor]=korisnik_podaci
     return svi_korisnici
@@ -65,13 +104,13 @@ Funkcija koja čuva podatke o svim korisnicima u fajl na zadatoj putanji sa zada
 """ #GOTOVO
 def sacuvaj_korisnike(putanja: str, separator: str, svi_korisnici: dict):
     if not type(svi_korisnici) is dict:
+        print("Greska")
         return "Greska: svi_korisnici nije dict"
 
     with open(putanja,'w') as f:
         for korisnik in svi_korisnici.values():
             nov_red=list(korisnik.values())
-            #print(nov_red)
-            nov_red=separator.join(nov_red)+'\n'
+            nov_red=separator.join(str(val) for val in nov_red)+'\n'
             f.write(nov_red)
 
 """
@@ -87,12 +126,12 @@ def ucitaj_korisnike_iz_fajla(putanja: str, separator: str) -> dict:  #GOTOVO
         red=red.rstrip('\n')
         red=red.split(separator)
         korisnik_podaci={
-                         'korisnicko_ime':red[0],'lozinka':red[1] , 'ime':red[2] ,
-                         'prezime':red[3] , 'email':red[4] ,
-                         'pasos':red[5], 'drzavljanstvo':red[6],
-                         'telefon':red[7],'pol':red[8],
+                         'ime':red[0] , 'prezime':red[1] ,
+                         'korisnicko_ime':red[2],'lozinka':red[3] , 'email':red[4] ,
+                         'pasos':int(red[5]), 'drzavljanstvo':red[6],
+                         'telefon':int(red[7]),'pol':red[8],
                          'uloga':red[9]}
-        korisnici_return[red[0]]=korisnik_podaci
+        korisnici_return[red[2]]=korisnik_podaci
     return korisnici_return
 
 
@@ -107,17 +146,15 @@ def login(svi_korisnici, korisnicko_ime, lozinka) -> dict:
         if korisnik_podaci["lozinka"]==lozinka:
             return korisnik_podaci
         else:
-            return "Pogresna sifra"
+            return "Login pogrešna lozinka"
     except KeyError:
         #print("Korisnik nije pronadjen")
-        return "Greska: Korisnik nije pronadjen"
+        return "Login nepostojeći"
 
 
 if __name__=="__main__":
-    svi_korisnici= ucitaj_korisnike_iz_fajla("./test_korisnici.csv",',')
-    svi_korisnici=kreiraj_korisnika(svi_korisnici,False,konstante.ULOGA_PRODAVAC,'Prodavac1','prodajemAVIONE123!','Pera','Peric')
-    ret=sacuvaj_korisnike("./test_korisnici.csv",',',svi_korisnici)
-    for korisnik in svi_korisnici.values():
-        print(korisnik)
-
-
+    svi_korisnici=ucitaj_korisnike_iz_fajla('./test_korisnici.csv','|')
+    print(svi_korisnici)
+    #svi_korisnici={}
+    #svi_korisnici=kreiraj_korisnika(svi_korisnici, False, konstante.ULOGA_KORISNIK, 'Lija321','lisica2003', 'Dejan', 'Lisica', 'lisica.dejan@hotmail.com',123456789, 'Srpsko', '063595793', 'm')
+    #sacuvaj_korisnike('./test_korisnici.csv','|',svi_korisnici)
